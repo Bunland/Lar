3cb61145843d1dd7775452f67dc39ae581604d07
vitalspace <myglobalsounds@gmail.com>
2023-11-06 19:16:44 -0600

    updated

--- main.c
+++ main.c
@@ -7,20 +7,6 @@ void Apis(JSContextRef context, JSObjectRef globalObject) {
     createCustomFunction(context, globalObject, "Add", Add);
 }
 
-// int main() {
-//     JSGlobalContextRef context = JSGlobalContextCreate(NULL);
-//     JSObjectRef globalObject = JSContextGetGlobalObject(context);
-
-//     Apis(context, globalObject);
-
-
-    
-//     // functionUno();
-//     // int result = functionDos(2, 2);
-//     // printf("El resultado de funcionDos es: %d\n", result);
-//     return 0;
-// }
-
 int main(int argc, char *argv[]) {
     JSGlobalContextRef context = JSGlobalContextCreate(NULL);
     JSObjectRef globalObject = JSContextGetGlobalObject(context);
@@ -30,12 +16,11 @@ int main(int argc, char *argv[]) {
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
             if(strcmp(argv[i], "run") == 0 && argv[2]){
-                // printf("%s", argv[1]);
                 interpreter(argv[2], context, globalObject);
             }
         }
     } else {
-        printf("No se han proporcionado argumentos.\n");
+        printf("No arguments provided.\n");
     }
 
     JSGlobalContextRelease(context);
